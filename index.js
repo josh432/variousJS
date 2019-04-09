3b9b2462e5ac7cc18503791e8dba8ab1bafc3308
Josh Silverman <jrsilverman71@hotmail.com>
2019-04-09 10:50:23 -0700

    first letter to uppercase

--- index.js
+++ index.js
@@ -73,7 +73,29 @@ function reverseInt(int) {
 // CHALLENGE 4: CAPITALIZE LETTERS
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
-function capitalizeLetters(str) {}
+function capitalizeLetters(str) {
+  //   const strArr = str.toLowerCase().split(" ");
+
+  //   for (let i = 0; i < strArr.length; i++) {
+  //     strArr[i] =
+  //       strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1);
+  //   }
+  //   return strArr.join(" ");
+
+  /////////////////////////////////////////
+
+  //   return str
+  //     .toLowerCase()
+  //     .split(" ")
+  //     .map(word => word[0].toUpperCase() + word.substr(1))
+  //     .join(" ");
+
+  ///////////////////////////////////////////
+
+  return str.replace(/\b[a-z]/gi, function(char) {
+    return char.toUpperCase();
+  });
+}
 
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
@@ -85,6 +107,6 @@ function maxCharacter(str) {}
 function fizzBuzz() {}
 
 // Call Function
-const output = reverseInt(12345);
+const output = capitalizeLetters("i love javascript");
 
 console.log(output);
